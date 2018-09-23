5aa0ceaab28c843a1ec31921fb5765859757448c
ishiy <y.ishihara.1993@gmail.com>
2018-09-23 12:43:11 +0900

    Update examples

--- examples/diffusion3/main.c
+++ examples/diffusion3/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include <mpi.h>
 #include "diffusion3.h"
 
 #define T_MAX 100
@@ -75,10 +74,8 @@ void writeData(Formura_Navi n) {
 }
 
 int main(int argc, char **argv) {
-  MPI_Init(&argc,&argv);
-
   Formura_Navi n;
-  Formura_Init(&n, MPI_COMM_WORLD);
+  Formura_Init(argc, argv, &n);
   setup(n);
 
   writeData(n);
@@ -87,6 +84,6 @@ int main(int argc, char **argv) {
     writeData(n);
   }
 
-  MPI_Finalize();
+  Formura_Finalize();
   return 0;
 }
