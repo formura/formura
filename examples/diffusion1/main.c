fb2d7fd00c7e4a594680b86373b0724cf9b19eae
ishiy <y.ishihara.1993@gmail.com>
2018-09-26 18:59:42 +0900

    Update examples

--- examples/diffusion1/main.c
+++ examples/diffusion1/main.c
@@ -4,19 +4,6 @@
 
 #define T_MAX 100
 
-double gauss(double x, double x0) {
-  return exp(-pow((x-x0)/(5*dx),2.0));
-}
-
-// 初期条件の設定
-void setup(Formura_Navi n) {
-  double x0 = n.length_x/2.0;
-  for(int ix = n.lower_x; ix < n.upper_x; ix++) {
-    double x = to_pos_x(ix,n);
-    formura_data.q[ix] = gauss(x,x0);
-  }
-}
-
 void writeData(Formura_Navi n) {
   char fn[256];
   sprintf(fn, "data/%d_%d.dat", n.time_step, n.my_rank);
@@ -32,7 +19,6 @@ void writeData(Formura_Navi n) {
 int main(int argc, char **argv) {
   Formura_Navi n;
   Formura_Init(&argc, &argv, &n);
-  setup(n);
 
   writeData(n);
   while(n.time_step < T_MAX) {
--- examples/diffusion3/main.c
+++ examples/diffusion3/main.c
@@ -4,27 +4,6 @@
 
 #define T_MAX 100
 
-double gauss(double x, double y, double z, double x0, double y0, double z0) {
-  return exp(-pow((x-x0)/(5*dx),2.0)-pow((y-y0)/(5*dy),2.0)-pow((z-z0)/(5*dz),2.0));
-}
-
-// 初期条件の設定
-void setup(Formura_Navi n) {
-  double x0 = n.length_x/2.0;
-  double y0 = n.length_y/2.0;
-  double z0 = n.length_z/2.0;
-  for(int ix = n.lower_x; ix < n.upper_x; ix++) {
-    double x = to_pos_x(ix,n);
-    for(int iy = n.lower_y; iy < n.upper_y; iy++) {
-      double y = to_pos_y(iy,n);
-      for(int iz = n.lower_z; iz < n.upper_z; iz++) {
-        double z = to_pos_z(iz,n);
-        formura_data.q[ix][iy][iz] = gauss(x,y,z,x0,y0,z0);
-      }
-    }
-  }
-}
-
 void writeData(Formura_Navi n) {
   int t = n.time_step;
   double x0 = n.length_x/2.0;
@@ -76,7 +55,6 @@ void writeData(Formura_Navi n) {
 int main(int argc, char **argv) {
   Formura_Navi n;
   Formura_Init(&argc, &argv, &n);
-  setup(n);
 
   writeData(n);
   while(n.time_step < T_MAX) {
