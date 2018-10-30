1cbbb05bd8a18a7add234b1f4289c0331f3bb87f
ishiy <y.ishihara.1993@gmail.com>
2018-10-30 16:48:46 +0900

    Add perturbate

--- examples/pearson2/main.c
+++ examples/pearson2/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include "pearson2.h"
 
 #define T_MAX 5000
 #define T_MON 100
+
+double frand() {
+  return rand()/((double)RAND_MAX);
+}
+
+void perturbate(Formura_Navi n) {
+  for(int ix = n.lower_x; ix < n.upper_x; ix++) {
+    for(int iy = n.lower_y; iy < n.upper_y; iy++) {
+      if (formura_data.u[ix][iy] != 1.0) {
+        formura_data.u[ix][iy] += 0.01*frand();
+        formura_data.v[ix][iy] += 0.01*frand();
+      }
+    }
+  }
+}
 
 void writeData(Formura_Navi n) {
   char fn[256];
@@ -21,8 +37,11 @@ void writeData(Formura_Navi n) {
 }
 
 int main(int argc, char **argv) {
+  srand(time(NULL));
   Formura_Navi n;
   Formura_Init(&argc, &argv, &n);
+
+  perturbate(n);
 
   writeData(n);
   while(n.time_step < T_MAX) {
