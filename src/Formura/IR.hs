203afc7bfa9fd84cb5641805005be63348758cc0
ishiy <y.ishihara.1993@gmail.com>
2018-11-02 19:20:19 +0900

    Fix bug

--- src/Formura/IR.hs
+++ src/Formura/IR.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
 module Formura.IR where
 
 import Control.Lens hiding (op)
@@ -28,7 +29,7 @@ instance Read IRNodeID where
 data IRGraph = IRGraph
   { tmpArrays :: [(String,OMNodeType,Int)]
   , kernels :: M.Map IRNodeID (Int,MMNode)
-  }
+  } deriving Show
 
 -- IRProgram is MMProgram whose manifest nodes are merged
 data IRProgram = IRProgram
@@ -70,9 +71,6 @@ mkIRGraph mmg = IRGraph { tmpArrays = tmps
     tmps = gatherTmps tmpName getSize mmg
     mmg' = insertMNSotres tmpName mmg
 
-    -- 一時変数の特定とStoreの発行 & LoadCursorをLoadCursorStaticに
-    -- マージ可能なMMNodeを集める
-    -- マージする
     g = M.fromList $ zipWith (\i n -> (IRNodeID i,n)) [0..] $ map (fmap (updateLoadCursor tmpName getSize . mergeMMNodes)) $ groupMMNodes getSize mmg'
 
 -- Manifestノードの配列サイズを計算する
@@ -88,8 +86,9 @@ calcSizes = M.foldlWithKey (\acc k (Node mi _ _) -> M.insert k (worker mi acc) a
                                                     in  (n0+s'):acc
             go _ acc = acc
 
-            maximum' [] = 0
-            maximum' x = maximum x
+maximum' :: (Num a, Ord a) => [a] -> a
+maximum' [] = 0
+maximum' x = maximum x
 
 isVoid :: ElemTypeF `Matches` s => s -> Bool
 isVoid (ElemType "void") = True
@@ -120,19 +119,14 @@ groupMMNodes getSize mmg = foldr (\x acc -> add x acc) [] $ M.toAscList mmg
                      | otherwise = (s,g ++ [x]):gs
 
 mergeMMNodes :: [(OMNodeID, MMNode)] -> MMNode
-mergeMMNodes = foldl' (\(Node mm0 t a) (_,Node mm1 _ _) -> Node (merge mm0 mm1) t a) (Node M.empty (ElemType "void") [])
-  where
-    getMaxKey = maximum . M.keys
-    updateKey x = M.mapKeys (+x)
-    merge m0 m1 = let offset = getMaxKey m0
-                      m1' = updateKey offset m1
-                  in mergeMMInstruction m0 m1'
+mergeMMNodes = foldl' (\(Node mm0 t a) (_,Node mm1 _ _) -> Node (mergeMMInstruction mm0 mm1) t a) (Node M.empty (ElemType "void") [])
 
 -- m0 に m1 の各ノードを追加していく
 -- このとき、同じノードがm0に存在していれば、m1にあるキーを書き換える
 mergeMMInstruction :: MMInstruction -> MMInstruction -> MMInstruction
 mergeMMInstruction m0 m1 = worker m0 $ M.toAscList m1
   where
+    offset = maximum' $ M.keys m0
     exist t m = let m' = M.filter (==t) m in if null m' then Nothing else Just (head $ M.keys m')
     fixKey old new n = case n ^. nodeInst of 
                          Uniop op k | k == old -> n & nodeInst .~ Uniop op new
@@ -146,11 +140,12 @@ mergeMMInstruction m0 m1 = worker m0 $ M.toAscList m1
                                            | k1 == old -> n & nodeInst .~ Triop op new k2 k3
                                            | k2 == old -> n & nodeInst .~ Triop op k1 new k3
                                            | k3 == old -> n & nodeInst .~ Triop op k1 k2 new
+                         Store v k | k == old -> n & nodeInst .~ Store v new
                          _ -> n
     worker m [] = m
     worker m ((mid,inst):ms) = case exist inst m of
-                                 Nothing -> worker (M.insert mid inst m) ms
-                                 Just k -> worker m (map (\(i,n) -> (mid, fixKey i k n)) ms)
+                                 Nothing -> let mid' = mid + offset in worker (M.insert mid' inst m) $ map (\(i,n) -> (i, fixKey mid mid' n)) ms
+                                 Just k -> worker m (map (\(i,n) -> (i, fixKey mid k n)) ms)
 
 -- LoadCursor を LoadCursorStatic に変更する
 updateLoadCursor :: (OMNodeID -> String) -> (OMNodeID -> Int) -> MMNode -> MMNode
