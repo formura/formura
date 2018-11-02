4251dbc66229df2037368b3bb9fb302b962c5035
ishiy <y.ishihara.1993@gmail.com>
2018-11-02 19:20:28 +0900

    Add debug info

--- app/formura.hs
+++ app/formura.hs
@@ -91,8 +91,21 @@ codegen sugarcoated_prog = do
     mapM_ pprMMNode $ M.toList (mmProg ^. omStepGraph)
     putStrLn ""
 
-  putStrLn "Generating code..."
   let irProg = genIRProgram mmProg
+  when (?commandLineOption ^. verbose) $ do
+    putStrLn "## Debug print: simulation state"
+    print (irProg ^. irStateSignature)
+    putStrLn ""
+
+    putStrLn "## Debug print: manifested init graph"
+    print (irProg ^. irInitGraph)
+    putStrLn ""
+
+    putStrLn "## Debug print: manifested step graph"
+    print (irProg ^. irStepGraph)
+    putStrLn ""
+
+  putStrLn "Generating code..."
   genCode irProg
 
 pprNode :: (OMNodeID, OMNode) -> IO ()
