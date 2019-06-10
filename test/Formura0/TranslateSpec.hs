ddfa922f639e11b6264a517849cdab08cc560d74
ishiy <y.ishihara.1993@gmail.com>
2019-06-10 20:25:18 +0900

    Add tests for findGlobalVariables

--- test/Formura0/TranslateSpec.hs
+++ test/Formura0/TranslateSpec.hs
@@ -7,18 +7,23 @@ import           Test.Hspec
 import Formura0.Frontend.Lexer
 import Formura0.Middleend.Translate
 import Formura0.Syntax
+import Formura0.Vec
 
 spec :: Spec
 spec = do
-  describe "makeTable" $ do
+  describe "makeIdentTable" $ do
     it "check a simple case" $
-      makeTable [ VarDecl (AlexPn 1 1 1) (IdentL "x") (ImmR 1.0)
+      makeIdentTable [ VarDecl (AlexPn 1 1 1) (IdentL "x") (ImmR 1.0)
                 , VarDecl (AlexPn 2 2 2) (TupleL [IdentL "a",IdentL "b"]) (Binop Add (IdentR "x") (IdentR "y"))]
         `shouldBe` Right (HM.fromList [("x", (AlexPn 1 1 1, ImmR 1.0)),("a",(AlexPn 2 2 2, AppR (Binop Add (IdentR "x") (IdentR "y")) (ImmR 0))),("b",(AlexPn 2 2 2, AppR (Binop Add (IdentR "x") (IdentR "y")) (ImmR 1)))])
     it "check a complex case" $
-      makeTable [ VarDecl (AlexPn 1 1 1) (TupleL [IdentL "a", TupleL [IdentL "b", IdentL "c"], IdentL "d"]) (IdentR "x")]
+      makeIdentTable [ VarDecl (AlexPn 1 1 1) (TupleL [IdentL "a", TupleL [IdentL "b", IdentL "c"], IdentL "d"]) (IdentR "x")]
         `shouldBe` Right (HM.fromList [ ("a", (AlexPn 1 1 1, AppR (IdentR "x") (ImmR 0)))
                                       , ("b", (AlexPn 1 1 1, AppR (AppR (IdentR "x") (ImmR 1)) (ImmR 0)))
                                       , ("c", (AlexPn 1 1 1, AppR (AppR (IdentR "x") (ImmR 1)) (ImmR 1)))
                                       , ("d", (AlexPn 1 1 1, AppR (IdentR "x") (ImmR 2)))
                                       ])
+  describe "findGlobalVariables" $ do
+    it "check a simple case" $
+      findGlobalVariables (HM.singleton "init" (AlexPn 1 1 1, LambdaR (TupleL []) (LetR [TypeDecl (AlexPn 1 1 1) (ModifiedType [] (GridT (Vec []) (IdentT "double"))) (IdentL "u"), VarDecl (AlexPn 1 1 1) (IdentL "u") (ImmR 0), TypeDecl (AlexPn 2 2 2) (ModifiedType [] (GridT (Vec []) (IdentT "double"))) (IdentL "v"), VarDecl (AlexPn 2 2 2) (IdentL "v") (ImmR 0)] (TupleR [IdentR "u", IdentR "v"]))))
+        `shouldBe` Right [("u", GridT (Vec []) (IdentT "double")),("v", GridT (Vec []) (IdentT "double"))]
